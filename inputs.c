3ad67e45b385d2eae79226faec5789cf6b144817
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2022-10-06 19:16:39 +0530

    Create inputs.c

---
+++ inputs.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+   // Declare the initialize variables
+
+   int number = 10;
+   float amount = 525.85;
+   char letter = 'V';
+   double pi = 3.142;
+   long int population_of_india = 10000000000;
+   char message[] = "Hi Friends!";
+
+   // Print the values of variables
+
+   printf("\nNumber = %d \nAmount = %f \nLetter = %c \nPopulation of India = %ld \nMessage = %s",
+          number, amount, letter, pi, population_of_india, message);
+
+    return 0;
+}
