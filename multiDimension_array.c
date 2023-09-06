130c4f81b7b1ae8855d763d4d2d39bdb9b831c8e
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2023-09-06 23:37:41 +0530

    Update multiDimension_array.c
    *Write a program to read and display a 2x2x2 array.

--- multiDimension_array.c
+++ multiDimension_array.c
@@ -5,19 +5,19 @@ int main(){
     for(i=0;i<2;i++){
         for(j=0;j<2;j++){
             for(k=0;k<2;k++){
-            printf("arra[%d][%d][%d] = ", i,j,k);
+            printf("array[%d][%d][%d] = ", i,j,k);
             scanf("%d", &array[i][j][k]);
             }
         }
     }
     
-    printf("\n The marix is: ");
+    printf("The elements of the 2x2x2 array are: \n");
         for(i=0;i<2;i++){
             printf("\n");
         for(j=0;j<2;j++){
             printf("\n");
             for(k=0;k<2;k++){
-            printf("arra[%d][%d][%d] = %d\t", i,j,k,array[i][j][k]);
+            printf("array[%d][%d][%d] = %d\t", i,j,k,array[i][j][k]);
             }
         }
     }
