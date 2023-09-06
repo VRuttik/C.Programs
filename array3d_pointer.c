ae3a7da25fcef0480b5ed37a9589dc29f296e879
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2023-09-06 23:35:33 +0530

    Create array3d_pointer.c
    *Write a program which illustrate the use of a pointer to a three - dimension array.

---
+++ array3d_pointer.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int array[2][2][2], i, j, k;
+    int (*parr)[2][2] = array;
+    printf("\n Enter the elements of the matrix: \n");
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            for(k=0;k<2;k++){
+            printf("array[%d][%d][%d] = ", i,j,k);
+            scanf("%d", &array[i][j][k]);
+            }
+        }
+    }
+    
+    printf("\n The elements of the 2x2x2 array are: \n");
+        for(i=0;i<2;i++){
+            printf("\n");
+        for(j=0;j<2;j++){
+            printf("\n");
+            for(k=0;k<2;k++){
+            printf("array[%d][%d][%d] = %d\t", i,j,k,*(*(*(parr+i)+j)+k));
+            }
+        }
+    }
+    return 0;
+}
